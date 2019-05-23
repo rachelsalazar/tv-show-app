f0e920045c40e54797b83e9babf09a199f6766b3
Rachel Salazar <ret1992@gmail.com>
2019-05-22 18:36:46 -0700

    added tv show component and linked to html

---
+++ src/app/tv-show/tv-show.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tv-show',
+  templateUrl: './tv-show.component.html',
+  styleUrls: ['./tv-show.component.css']
+})
+export class TvShowComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
