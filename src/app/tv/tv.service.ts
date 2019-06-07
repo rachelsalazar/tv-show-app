3347d281823e04cc56ae00bb840c7bcd102bf83b
Rachel Salazar <ret1992@gmail.com>
2019-06-06 19:32:14 -0700

    wrote receiving end interface within tv service.

--- src/app/tv/tv.service.ts
+++ src/app/tv/tv.service.ts
@@ -1,6 +1,20 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+interface ITvShowData {
+  name: string,
+  image: {
+    medium: string
+  },
+  summary: string,
+  schedule: {
+    days: string
+  },
+  rating: {
+    average: number
+  }
+}
+
 @Injectable({
   providedIn: 'root'
 })
