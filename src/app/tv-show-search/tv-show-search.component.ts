fa69d556e6b8f76c1b8e8e28710acf6fa58f8807
Rina Yang <42502822+didflsk@users.noreply.github.com>
2019-06-08 01:06:02 -0700

    function for user input, import tvservice,timer

--- src/app/tv-show-search/tv-show-search.component.ts
+++ src/app/tv-show-search/tv-show-search.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { TvService } from '../tv/tv.service';
+import { debounceTime } from 'rxjs/operators';
 
 @Component({
   selector: 'app-tv-show-search',
@@ -9,9 +11,17 @@ import { FormControl } from '@angular/forms';
 export class TvShowSearchComponent implements OnInit {
   search = new FormControl()
 
-  constructor() { }
+  constructor(private tvService: TvService) { }
 
   ngOnInit() {
+    this.search.valueChanges
+    .pipe(debounceTime(1000))
+    .subscribe((searchValue : string) => {
+      if (searchValue) {
+        const userInput = searchValue.split(',').map(s => s.trim());
+        this.tvService.getTvShow
+      }
+    })
   }
 
 }
