ad0355287d9a1ba390b100fdf1fdd7ec21e6c63c
Rachel Salazar <ret1992@gmail.com>
2019-07-01 12:40:50 -0700

    fixed 4 AppComponent errors, added material

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,15 +1,30 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { MatFormFieldModule, MatInputModule, MatButtonModule, MatToolbarModule, MatIconModule, MatCardModule} from '@angular/material';
+import { TvShowSearchComponent } from './tv-show-search/tv-show-search.component';
+import { TvShowComponent } from './tv-show/tv-show.component';
+import { FormsModule, ReactiveFormsModule} from '@angular/forms';
+
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatButtonModule,
+        MatToolbarModule,
+        MatIconModule,
+        MatCardModule,
+        FormsModule,
+        ReactiveFormsModule,
       ],
       declarations: [
-        AppComponent
+        AppComponent,
+        TvShowSearchComponent,
+        TvShowComponent
       ],
     }).compileComponents();
   }));
